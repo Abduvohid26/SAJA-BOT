2da71a757203a09199dfb8ed0c9245db9ef676a4
Abduvohid26 <abdujalilovabduvohid6@gmail.com>
2024-10-14 16:36:54 +0500

    fix

--- data/config.py
+++ data/config.py
@@ -1,8 +1,7 @@
 from environs import Env
 env = Env()
 env.read_env()
-# BOT_TOKEN='7784958688:AAHB4GfO7YmrNL4kNDIkm7zd1q0n7pYoQkA'
-BOT_TOKEN='7409681510:AAGcu6p1411vNiardtcmZUUmUSxij7slXWI'
+BOT_TOKEN='7262239509:AAEAZIPHjBAXIZQ5Nr2cTXgbqUJ_8-BzzXc'
 ADMINS=env.list('ADMINS')
 # SEO=env.list('SEO')
 SEO = ['147737693', '816660001']
